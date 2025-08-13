58e5f985fec0bc288fede96d5961ba04307bf2c1
AleMora <158232771+asapAle@users.noreply.github.com>
2025-08-13 15:25:35 -0300

    Add files via upload

--- DON'T TAP THE GLASS/game_over.gd
+++ DON'T TAP THE GLASS/game_over.gd
@@ -1,5 +1,14 @@
 extends Control
 
+@onready var label_score = $VBoxContainer/LabelScore # Ajustá según tu escena
+
+func _ready():
+	# Mostrar puntaje de la partida
+	var s = 0
+	if get_tree().has_meta("ultimo_score"):
+		s = int(get_tree().get_meta("ultimo_score"))
+	label_score.text = "Puntaje: " + str(s)
+
 func _on_reintentar_pressed() -> void:
 	if get_tree().has_meta("ultima_escena"):
 		get_tree().change_scene_to_file(get_tree().get_meta("ultima_escena"))
